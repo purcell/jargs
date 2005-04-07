71591aef0c2741285078ef754a8750140b597d41
Ewan Mellor <jargs@ewanmellor.org.uk>
2005-04-07 14:36:50 +0000

    Added testDefaults to test getOptionValue(Option, Object), testMultipleValues to test getOptionValue used in a loop to get multiple uses, and testGetOptionValues to test getOptionValues().

--- src/jargs/test/gnu/CmdLineParserTestCase.java
+++ src/jargs/test/gnu/CmdLineParserTestCase.java
@@ -2,7 +2,9 @@ package jargs.test.gnu;
 
 import jargs.gnu.CmdLineParser;
 
+import java.util.Enumeration;
 import java.util.Locale;
+import java.util.Vector;
 
 import junit.framework.TestCase;
 
@@ -35,6 +37,115 @@ public class CmdLineParserTestCase extends TestCase {
 				.doubleValue(), 0.1e-6);
 		assertArrayEquals(new String[]{"rest"}, parser.getRemainingArgs());
 	}
+
+
+    public void testDefaults() throws Exception {
+		CmdLineParser parser = new CmdLineParser();
+		CmdLineParser.Option boolean1 = parser.addBooleanOption("boolean1");
+		CmdLineParser.Option boolean2 = parser.addBooleanOption("boolean2");
+		CmdLineParser.Option boolean3 = parser.addBooleanOption("boolean3");
+		CmdLineParser.Option boolean4 = parser.addBooleanOption("boolean4");
+		CmdLineParser.Option boolean5 = parser.addBooleanOption("boolean5");
+
+		CmdLineParser.Option int1 = parser.addIntegerOption("int1");
+		CmdLineParser.Option int2 = parser.addIntegerOption("int2");
+		CmdLineParser.Option int3 = parser.addIntegerOption("int3");
+		CmdLineParser.Option int4 = parser.addIntegerOption("int4");
+
+		CmdLineParser.Option string1 = parser.addStringOption("string1");
+		CmdLineParser.Option string2 = parser.addStringOption("string2");
+		CmdLineParser.Option string3 = parser.addStringOption("string3");
+		CmdLineParser.Option string4 = parser.addStringOption("string4");
+
+		parser.parse(new String[] {
+          "--boolean1", "--boolean2",
+          "--int1=42", "--int2=42",
+          "--string1=Hello", "--string2=Hello",
+        });
+
+		assertEquals(Boolean.TRUE, parser.getOptionValue(boolean1));
+		assertEquals(Boolean.TRUE,
+                     parser.getOptionValue(boolean2, Boolean.FALSE));
+		assertEquals(null, parser.getOptionValue(boolean3));
+		assertEquals(Boolean.FALSE,
+                     parser.getOptionValue(boolean4, Boolean.FALSE));
+		assertEquals(Boolean.TRUE,
+                     parser.getOptionValue(boolean5, Boolean.TRUE));
+
+        Integer forty_two  = new Integer(42);
+        Integer thirty_six = new Integer(36);
+
+		assertEquals(forty_two,  parser.getOptionValue(int1));
+		assertEquals(forty_two,  parser.getOptionValue(int2, thirty_six));
+		assertEquals(null,       parser.getOptionValue(int3));
+		assertEquals(thirty_six, parser.getOptionValue(int4, thirty_six));
+
+		assertEquals("Hello",   parser.getOptionValue(string1));
+		assertEquals("Hello",   parser.getOptionValue(string2, "Goodbye"));
+		assertEquals(null,      parser.getOptionValue(string3));
+		assertEquals("Goodbye", parser.getOptionValue(string4, "Goodbye"));
+    }
+
+
+	public void testMultipleUses() throws Exception {
+		CmdLineParser parser = new CmdLineParser();
+		CmdLineParser.Option verbose = parser.addBooleanOption('v', "verbose");
+		CmdLineParser.Option foo = parser.addBooleanOption('f', "foo");
+		CmdLineParser.Option bar = parser.addBooleanOption('b', "bar");
+
+		parser.parse(new String[] {
+          "--foo", "-v", "-v", "--verbose", "-v", "-b", "rest"
+        });
+
+        int verbosity = 0;
+        while (true) {
+            Boolean b = (Boolean)parser.getOptionValue(verbose);
+
+            if (b == null) {
+                break;
+            }
+
+            if (b == Boolean.TRUE) {
+                verbosity++;
+            }
+            else {
+                assertEquals(Boolean.FALSE, b);
+                verbosity--;
+            }
+        }
+
+        assertEquals(4, verbosity);
+    }
+
+
+	public void testGetOptionValues() throws Exception {
+		CmdLineParser parser = new CmdLineParser();
+		CmdLineParser.Option verbose = parser.addBooleanOption('v', "verbose");
+		CmdLineParser.Option foo = parser.addBooleanOption('f', "foo");
+		CmdLineParser.Option bar = parser.addBooleanOption('b', "bar");
+
+		parser.parse(new String[] {
+          "--foo", "-v", "-v", "--verbose", "-v", "-b", "rest"
+        });
+
+        int verbosity = 0;
+        Vector v = parser.getOptionValues(verbose);
+        Enumeration e = v.elements();
+        while (e.hasMoreElements()) {
+            Boolean b = (Boolean)e.nextElement();
+
+            if (b == Boolean.TRUE) {
+                verbosity++;
+            }
+            else {
+                assertEquals(Boolean.FALSE, b);
+                verbosity--;
+            }
+        }
+
+        assertEquals(4, verbosity);
+    }
+
 
 	public void testBadFormat() throws Exception {
 		CmdLineParser parser = new CmdLineParser();
